2a4ae891f0a2a4fb4ea759597b3a4a86d8d721b0
pver <peetvermeulen@gmail.com>
2017-12-07 22:04:11 +0100

    Challenge07: cleanup

--- challenge07.fsx
+++ challenge07.fsx
@@ -1,6 +1,7 @@
 open System
 open System.Collections.Generic
 
+let inputChallenge7Path = "inputChallenge07.txt"
 let splitBy (separator:string) (s:string)  = s.Split([|separator|],StringSplitOptions.RemoveEmptyEntries)
 let splitByArrow = splitBy " -> "
 let splitByComma = splitBy ", "
@@ -11,66 +12,56 @@ type TowerItem = {name:string; weight:int; items:string[]}
 let parseItems (inputPath:string) =
     inputPath 
     |> System.IO.File.ReadAllLines 
-    |> Seq.map (splitByArrow >> (fun x -> 
-                    let (name, weight) = x.[0]|>splitNameAndWeight
-                    let upperItems = if(x.Length)>1 then (x.[1]|>splitByComma) else [||] 
-                    {name=name; weight=weight; items=upperItems}
-                    ))
-    |> Seq.toArray
-
+    |> Array.map (splitByArrow >> (fun x -> 
+                    match x.Length with
+                    | 1 | 2 -> let (name, weight) = x |> Array.head |> splitNameAndWeight                  
+                               let subItems = match x |> Array.tail with
+                                               | [|subItems|] -> subItems|>splitByComma
+                                               | _ ->  [||] 
+                               {name=name; weight=weight; items=subItems}
+                    | _ -> failwith "invalid item found"
+                    ) )
 
 let findBottom (items:TowerItem[]) =
-    let itemsNoTops =
-        items
-        |> Array.filter (fun x -> not (Array.isEmpty x.items))
-    let candidates = itemsNoTops |> Array.map (fun x -> x.name)
-    let upperItems = itemsNoTops |> Array.collect (fun x -> x.items)
-    candidates |> Array.except upperItems |> Array.head // should contain only one: the one and only bottom
-
-let inputChallenge7Path = "inputChallenge07.txt"
+    let candidates = items |> Array.map (fun x -> x.name)
+    let subItemNames = items |> Array.collect (fun x -> x.items)
+    candidates |> Array.except subItemNames |> Array.exactlyOne // should contain only one: the one and only bottom
 
 parseItems inputChallenge7Path |> findBottom |> printfn "Challenge 7A: %s"
 
-let solve () =
-    let startItems = parseItems inputChallenge7Path
-    
+let solveChallenge (startItems:TowerItem[]) =
     let calculated = new Dictionary<string, int>()
-    
-    let rec loop (items:TowerItem[]) =
 
+    let rec loop (items:TowerItem[]) =
         let resultsFound = 
             items 
             |> Array.map (fun item -> 
-                                match Array.isEmpty item.items with
-                                | true -> 
-                                            calculated.Add(item.name, item.weight) 
-                                            Option.None
-                                | false -> 
-                                            if (item.items|>Seq.forall calculated.ContainsKey) 
-                                            then
-                                                let weights = item.items |> Seq.map (fun item -> calculated.[item])
-                                                let allSameWeights = (weights |> Seq.distinct |> Seq.length)=1
-                                                match allSameWeights with
-                                                | true -> 
-                                                            calculated.Add(item.name, item.weight + (weights|>Seq.sum))
-                                                            Option.None
-                                                | false -> 
-                                                        let (wrongWeight, i) = weights 
-                                                                                |> Seq.countBy (fun x -> x) 
-                                                                                |> Seq.filter (fun (_, occurrence)-> occurrence=1) 
-                                                                                |> Seq.map (fun (value, _)-> value) 
-                                                                                |> Seq.head
-                                                                                |> fun x -> (x, weights |> Seq.findIndex ((=) x))
-                                                        let correctWeight = weights |> Seq.filter (fun x -> x <> wrongWeight) |> Seq.head
-                                                        let tooMuchWeight = (wrongWeight-correctWeight)
+                                match item.items with
+                                | [||] -> calculated.Add(item.name, item.weight) 
+                                          Option.None
+                                | subItems -> 
+                                            match (subItems|>Seq.forall calculated.ContainsKey) with
+                                            | false -> Option.None
+                                            | true ->
+                                                    let weights = subItems |> Array.map (fun item -> calculated.[item])
+                                                    let allSameWeights = (weights |> Seq.distinct |> Seq.length)=1
+                                                    match allSameWeights with
+                                                    | true -> calculated.Add(item.name, item.weight + (weights|>Seq.sum))
+                                                              Option.None
+                                                    | false -> 
+                                                            let (wrongWeight, i) = weights 
+                                                                                    |> Seq.countBy id
+                                                                                    |> Seq.filter (fun (_, occurrence)-> occurrence=1) 
+                                                                                    |> Seq.map (fun (value, _)-> value) 
+                                                                                    |> Seq.head
+                                                                                    |> fun x -> (x, weights |> Seq.findIndex ((=) x))
+                                                            let correctWeight = weights |> Seq.filter ((<>) wrongWeight) |> Seq.head
+                                                            let overWeight = (wrongWeight-correctWeight)
 
-                                                        let misbehavingItem = startItems |> Array.filter (fun x -> x.name = (item.items.[i])) |> Array.head
-                                                        
-                                                        let expectedWeight = (misbehavingItem.weight-tooMuchWeight)
-                                                        Option.Some(expectedWeight)
-
-                                            else 
-                                                Option.None
+                                                            let misbehavingItem = startItems |> Array.filter (fun x -> x.name = (subItems.[i])) |> Array.head
+                                                            
+                                                            let expectedWeight = (misbehavingItem.weight-overWeight)
+                                                            Option.Some(expectedWeight)
                             )
 
             |> Array.filter (Option.isSome)
@@ -82,4 +73,4 @@ let solve () =
 
     loop startItems
 
-printfn "Challenge 7B: %d" <| solve ()
+printfn "Challenge 7B: %d" <| solveChallenge (parseItems inputChallenge7Path)
